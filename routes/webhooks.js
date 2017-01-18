f95a39de91e44a748d3025a2650c20ad4e11bd22
Hedroed <nathan.rydin@gmail.com>
2017-01-18 19:37:41 +0100

    Implement messenger bot

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var Planning = require('../planning');
 var moment = require('moment');
 moment.locale("fr");
+var request = require('request');
+
+var token = "EAAFq1GqUrIEBAELUVuua4YsHKChnbFZBip0ZAPIJbEh9bOIalrXqjVDib9YGvDg7h36VQfopGRswSoCDabRa6QCE0XoGZADfDjwOojwptzSyK4Y9OFmWGuMl2btW8ZBZAW6hj5UGz1QZBMK6TOpVYebFH2LIPX3EdOj5afYKicTAZDZD";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,7 +15,112 @@ router.get('/', function(req, res, next) {
     
     console.log(req.query);
     
-    res.sendStatus(200);
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === "salut_je_suis_content") {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);          
+    }  
 });
+
+router.post('/', function (req, res) {
+  var data = req.body;
+  console.log(data);
+
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:", 
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    sendTextMessage(senderID, messageText);
+    
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+    //  text: messageText,
+      attachment:{
+          type:"image",
+          payload:{
+            url:"https://scontent.xx.fbcdn.net/v/t1.0-1/406650_436530976412124_479066119_n.jpg?oh=2338c87c46de9cfe9ea0d58c363a86d7&oe=5911CAC1"
+          }
+        }
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: token },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 module.exports = router;
